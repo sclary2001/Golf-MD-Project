2b30e3b13a3bdd0ad8945ed6a1329755174ce166
Clary <sclary@una.edu>
2026-09-15 14:03:59 -0500

    getting caught up?

---
+++ server/app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+
+app.use(express.static(join(__dirname, '../public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, 'public', 'index.html'));
+})
+
+
+app.listen(3000, () => console.log('http://localhost:3000'));
